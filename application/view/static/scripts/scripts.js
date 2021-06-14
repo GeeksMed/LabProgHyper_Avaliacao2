fa138c131bebbb92a16ab272f48743a9df72227e
Unknown <silva.germain@gmail.com>
2021-06-14 20:49:13 -0300

    Organizando código JS

--- application/view/static/scripts/scripts.js
+++ application/view/static/scripts/scripts.js
@@ -2,3 +2,15 @@ function mostraImagem(img) {
 		var imagem = document.getElementById("img_selected");
 		imagem.src = img;
 }
+
+var btn = document.querySelector("#botao_menu");
+btn.addEventListener("click", function() {
+    var div = document.querySelector("#dropdown-content");
+
+  if(div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+      div.style.display = "none";
+  }
+
+});
